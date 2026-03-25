625b10e136e6298e3c2ff6a1427a55426df53112
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2026-03-24 21:54:13 -0300

    feat: Criação dos metodos de CRUD do repository de usuario

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "@prisma/client/extension";
+import { Prisma } from "../generated/prisma/index.js";
 import type { users } from "../generated/prisma/index.js";
 import { prisma } from "../server.js";
 
@@ -16,5 +16,27 @@ export class UserRepository{
 
     async findAll(): Promise<users[]> {
         return await prisma.users.findMany();
+    }
+
+    async findById(user_uuid: string): Promise<users | null> {
+        return await prisma.users.findUnique({ where: { user_uuid } });
+    }
+
+    async createUser(userData: Prisma.usersCreateInput): Promise<users | null> {
+        const user = await prisma.users.create({ data: userData });
+        console.log("Usuário criado com sucesso: ", user);
+        return user;
+    }
+
+    async updateUser(user_uuid: string, userData: Prisma.usersUpdateInput): Promise<users | null> {
+        const updatedUser = await prisma.users.update({ where: { user_uuid }, data: userData });
+        console.log("Usuário atualizado com sucesso: ", updatedUser);
+        return updatedUser;
+      }
+
+    async deleteById(user_uuid: string): Promise<users | null> {
+        const deletedUser = await prisma.users.delete({ where: { user_uuid } });
+        console.log("Usuário removido com sucesso: ", deletedUser);
+        return deletedUser;
     }
 }
