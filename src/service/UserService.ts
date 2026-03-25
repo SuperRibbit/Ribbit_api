51b3176964b9608c4bba2de28d63dbc38fe61942
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2026-03-24 22:05:32 -0300

    feat: Criação da service com o CRUD de usuario

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,4 +1,4 @@
-import type { users } from "../generated/prisma/index.js";
+import type { Prisma, users } from "../generated/prisma/index.js";
 import { UserRepository } from "../repository/UserRepository.js";
 
 export class UserService{
@@ -7,4 +7,49 @@ export class UserService{
     async findAll(): Promise<users[]> {
         return await this.userRepository.findAll();
     }
+
+    async findById(id: string | undefined): Promise<users> {
+        if (!id) {
+          throw new Error("Usuário não encontrado");
+        }
+    
+        const user = await this.userRepository.findById(id);
+    
+        if (!user) {
+          throw new Error("Usuário não encontrado");
+        }
+        return user;
+    }
+
+    async createUser(userData: any): Promise<users | null> {
+        const { email, password, full_name, role, avatar_url } = userData;    
+    
+        const userCreateInput: Prisma.usersCreateInput = {
+          email,
+          password_hash: password,
+          full_name,
+          role,
+          avatar_url,
+        };
+    
+        return await this.userRepository.createUser(userCreateInput);
+    }
+    
+    async updateUser(id: string | undefined, userData: Prisma.usersUpdateInput): Promise<users | null> {
+        if (!id) {
+          throw new Error("Usuário não encontrado");
+        }
+
+        return await this.userRepository.updateUser(id, userData);
+    }
+
+    async deleteById(id: string | undefined): Promise<users | null> {
+        if (!id) {
+          throw new Error("Usuário não encontrado");
+        }
+    
+        return await this.userRepository.deleteById(id);
+    }
+    
+    
 }
