be964aca9bdd7502385b38ef226fd04a3a9f893d
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2026-04-03 11:52:02 -0300

    feat: Implementação do dto de upload de link

--- src/dto/ClassFileDtos.ts
+++ src/dto/ClassFileDtos.ts
@@ -3,3 +3,9 @@ import type { Class_file } from "../generated/prisma/index.js";
 export type ClassFileResponse = Omit<Class_file, "metadata"> & {
   metadata: Record<string, any> | null;
 };
+
+export interface UploadVideoLinkRequest {
+  class_id: number;
+  url: string;
+  display_name: string;
+}
