746d39e2ab256827e45cf32261b3869ac12a4940
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2026-04-07 22:35:10 -0300

    feat: Padronização dos erros em auth

--- src/middleware/tsoaAuth.ts
+++ src/middleware/tsoaAuth.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import jwt from "jsonwebtoken";
+import { AppError } from "../utils/AppError.js";
 
 const JWT_SECRET = process.env.JWT_SECRET || "supercraft";
 
@@ -12,7 +13,7 @@ export function expressAuthentication(
     const authHeader = request.headers.authorization;
 
     if (!authHeader) {
-      return Promise.reject(new Error("Token não fornecido!"));
+      return Promise.reject(new AppError("Token não fornecido!",401));
     }
 
     const parts = authHeader.split(" ");
@@ -21,17 +22,17 @@ export function expressAuthentication(
     const token = parts[1];
 
     if (parts.length !== 2 || !scheme || !token || !/^Bearer$/i.test(scheme)) {
-      return Promise.reject(new Error("Token malformatado!"));
+      return Promise.reject(new AppError("Token malformatado!",401));
     }
 
     return new Promise((resolve, reject) => {
       jwt.verify(token, JWT_SECRET, function (err: any, decoded: any) {
         if (err) {
-          reject(new Error("Token inválido!"));
+          reject(new AppError("Token inválido!",401));
         } else {
           if (scopes && scopes.length > 0) {
             if (!scopes.includes(decoded.role)) {
-              reject(new Error("Acesso negado! Apenas professores podem realizar esta ação."));
+              reject(new AppError("Acesso negado! Apenas professores podem realizar esta ação.",403));
               return;
             }
           }
