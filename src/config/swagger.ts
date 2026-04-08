149f8b6cb98846557f643aa26a3b2c8a07ce6609
vitu1415 <vitoralves0801@gmail.com>
2026-04-07 22:45:08 -0300

    add config swaguer

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -2,8 +2,12 @@ import swaggerUi from 'swagger-ui-express';
 import type { Express } from 'express';
 import path from 'path';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
 
-const swaggerFile = path.join(__dirname, '../../dist/swagger.json');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerFile = path.join(__dirname, '../generated/swagger.json');
 const swaggerSpec = JSON.parse(fs.readFileSync(swaggerFile, 'utf8'));
 
 export const setupSwagger = (app: Express) => {
