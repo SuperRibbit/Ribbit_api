ab7af7af8f3aef3b2a6e08402e6c95778514abd2
MizunoYudi <joaomizunovy@gmail.com>
2026-04-14 21:49:35 -0300

    feat: Adição dos métodos CRUD no repository de CourseClass

--- src/repository/CourseClassRepository.ts
+++ src/repository/CourseClassRepository.ts
@@ -5,9 +5,31 @@ export class CourseClassRepository {
     private static instance: CourseClassRepository;
 
     static getInstance(): CourseClassRepository {
-        if(!this.instance) {
+        if (!this.instance) {
             this.instance = new CourseClassRepository();
         }
         return this.instance;
+    }
+
+    async findByID(class_id: number): Promise<Course_class | null> {
+        return await prisma.course_class.findUnique({ where: { class_id } });
+    }
+
+    async create(data: Prisma.Course_classCreateInput): Promise<Course_class> {
+        const created = await prisma.course_class.create({ data });
+        console.log("Aula criada com sucesso: ", created);
+        return created;
+    }
+
+    async update(class_id: number, data: Prisma.Course_classUpdateInput): Promise<Course_class> {
+        const update = await prisma.course_class.update({ where: { class_id }, data});
+        console.log("Aula atualizada com sucesso: ", update);
+        return update;
+    }
+
+    async deleteById(class_id: number): Promise<Course_class> {
+        const deleted = await prisma.course_class.delete({ where: { class_id }});
+        console.log("Aula removida com sucesso: ", deleted);
+        return deleted;
     }
 }
