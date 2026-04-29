8bc0bb1bdddf4f197be42568b9df4b332510e2dc
MizunoYudi <joaomizunovy@gmail.com>
2026-04-28 22:52:58 -0300

    feat: Adição dos métodos de Busca por id, Atualização e Apagar CourseClass

--- src/controller/CourseClassController.ts
+++ src/controller/CourseClassController.ts
@@ -1,4 +1,4 @@
-import { Body, Post, Route, Security, SuccessResponse, Response, Tags, Controller } from "tsoa";
+import { Body, Post, Route, Security, SuccessResponse, Response, Tags, Controller, Get, Path, Put, Delete } from "tsoa";
 import type { CourseClassCreateRequest, CourseClassResponse, CourseClassUpdateRequest } from "../dto/CourseClassDto.js";
 import { CourseClassService } from "../service/CourseClassService.js";
 
@@ -21,5 +21,37 @@ export class CourseClassController extends Controller{
         }
         this.setStatus(201);
         return courseClass;
+    }
+
+    @Get("{id}")
+    @Response("404", "Aula não encontrada")
+    @Security("bearerAuth")
+    public async findById(@Path() id: number): Promise<CourseClassResponse>{
+        const courseClass = await this.courseClassService.findById(id);
+        return courseClass;
+    }
+
+    @Put("{id}")
+    @Response("400", "Erro ao atualizar a aula")
+    @Response("404", "Aula não encontrada")
+    @Security("bearerAuth", ["prof"])
+    public async updateCourseClass(@Path() id: number, @Body() requestBody: CourseClassUpdateRequest): Promise<CourseClassResponse>{
+        const courseClass = await this.courseClassService.updateCourseClass(id, requestBody);
+
+        if(!courseClass){
+            this.setStatus(404);
+            throw new Error("Aula não encontrada para atualização");
+        }
+        this.setStatus(200);
+        return courseClass;
+    }
+
+    @Delete("{id}")
+    @SuccessResponse("204", "Aula deletada com sucesso")
+    @Response("404", "Aula não encontrada")
+    @Security("bearerAuth", ["prof"])
+    public async deleteCourseClass(@Path() id: number): Promise<void>{
+        await this.courseClassService.deleteCourseClass(id);
+        this.setStatus(204);
     }
 }
